95990ba3c371d57ac4986cc4c0a0c11b4ec3aed1
pilathraj <pilathraj@gmail.com>
2021-04-20 17:31:49 +0530

    Update reverse.go

--- golang/strings/reverse.go
+++ golang/strings/reverse.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func reverse(str string) string {
+  var result string
+  for _, v:= range(str) {
+    result = v + result    
+  }
+  return result
+}
+
 func main() {
   s := []int{1,2,3}
   s1 := "Pilathraj"
@@ -14,4 +22,5 @@ func main() {
   }
   fmt.Println(s)
   fmt.Println(string(s2))
+  fmt.Println(reverse(s1))
 }
