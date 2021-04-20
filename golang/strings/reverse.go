dc7d44b8eeb373d710a45bde1e2693f979189bb3
pilathraj <pilathraj@gmail.com>
2021-04-20 17:27:11 +0530

    Create reverse.go

---
+++ golang/strings/reverse.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  s := []int{1,2,3}
+  s1 := "Pilathraj"
+  for i, j := 0, len(s)-1; i<j; i,j = i+1, j-1 {
+    s[i], s[j] = s[j], s[i]
+  }
+  s2 :=[]rune(s1)
+  for i, j := 0, len(s2)-1; i<j; i,j = i+1, j-1 {
+   s2[i], s2[j] = s2[j], s2[i]
+  }
+  fmt.Println(s)
+  fmt.Println(string(s2))
+}
