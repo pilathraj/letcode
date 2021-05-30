47e225a9dc73b9d219a9227068648f595eccd550
pilathraj <pilathraj@gmail.com>
2021-05-30 15:24:29 +0530

    Update main.go

--- golang/array/sort/main.go
+++ golang/array/sort/main.go
@@ -9,13 +9,13 @@ func main() {
 	a := [5]int{2,4,1,3,5}
 	s :=0
 	e := len(a)-1
-  for s<e {   // O(N)
-	  if a[s] > a[e]{
-		  a[s], a[e] = a[e], a[s]
-		  s +=1	  
-	  } else {
-	    e -= 1
-	  }
-  }
+	for s<e {   // O(N)
+		  if a[s] > a[e]{
+			  a[s], a[e] = a[e], a[s]
+			  s +=1	  
+		  } else {
+		    e -= 1
+		  }
+	}
 	fmt.Println(a)  //[1 2 4 3 5]	
 }
