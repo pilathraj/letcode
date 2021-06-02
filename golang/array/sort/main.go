729c06447a1e1c5e830cc93fd2c990a7d5995fd1
pilathraj <pilathraj@gmail.com>
2021-06-02 11:57:15 +0530

    Update main.go

--- golang/array/sort/main.go
+++ golang/array/sort/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func main() {
+/*func main() {
 	fmt.Println("Hello, playground")
 	a := [5]int{2,4,1,3,5}
 	s :=0
@@ -18,4 +18,17 @@ func main() {
 		  }
 	}
 	fmt.Println(a)  //[1 2 4 3 5]	
+}*/
+func main() {
+	a := []int{3, 4, 2, 1, 5}
+	n := len(a)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if a[i] > a[j] {
+				a[i], a[j] = a[j], a[i]
+			}
+		}
+	}
+	fmt.Println(a)
 }
+
